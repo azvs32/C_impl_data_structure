f6b5dda11180d504b58d84fefce782ce8153452d
azvs.top@gmail.com <azvs.top@qq.com>
2024-11-23 22:46:44 +0800

    优化了日志的判读，以及记录了日志可完善的部分

--- include/azvs_base.h
+++ include/azvs_base.h
@@ -6,6 +6,12 @@
 //////////////////////////////////////////////////
 //     日志系统
 //////////////////////////////////////////////////
+/*
+ * 一些设想：
+ *  + [简单]添加时间模块
+ *  + [简单]用户自定义日志输出格式
+ *  + 定义日志输出模式：console\file\both\...
+ */
 
 // 定义了日志有哪些级别，危险级别依次递增。
 // 若修改了该枚举，请同步修改 get_log_level 函数。
@@ -44,7 +50,11 @@ char *get_log_level(LogLevel level);
 void log_message(LogLevel level, const char *file, const char *function, int line, const char *format, ...);
 
 // 给外界简化日志打印操作
-#define LOG_MSG(level, format, ...) log_message(level, __FILE__, __FUNCTION__, __LINE__, format, ##__VA_ARGS__)
+// #define LOG_MSG(level, format, ...) log_message(level, __FILE__, __FUNCTION__, __LINE__, format, ##__VA_ARGS__)
+#define LOG_MSG(level, format, ...) \
+  do { if (level >= current_log_level) \
+    log_message(level, __FILE__, __FUNCTION__, __LINE__, format, ##__VA_ARGS__); \
+  } while(0)
 
 //////////////////////////////////////////////////
 //     内存申请与释放
@@ -81,7 +91,7 @@ void *realloc_with_log(void *ptr, size_t size, const char *file, const char *fun
 // 将 malloc、free、realloc 包装成带日志的版本
 #define NEW(type) ((type *)new_with_log(sizeof(type), __FILE__, __FUNCTION__, __LINE__))
 #define NEW_ARRAY(type, count) ((type *)new_with_log(sizeof(type) * (count), __FILE__, __FUNCTION__, __LINE__))
-#define DELETE(ptr) delete_with_log(ptr, __FILE__, __FUNCTION__, __LINE__)
 #define REALLOC(ptr, size) remalloc_with_log(ptr, size, __FILE__, __FUNCTION__, __LINE__)
+#define DELETE(ptr) delete_with_log(ptr, __FILE__, __FUNCTION__, __LINE__)
 
 #endif // !AZVS_BASE_H
