883c67e27b7499c222f719674e0505277c93d852
azvs.top@gmail.com <azvs.top@qq.com>
2024-12-01 11:23:53 +0800

    实现链表的初始化\释放\插入\删除\输出 函数

--- impl/azvs_linked.c
+++ impl/azvs_linked.c
@@ -1,3 +1,154 @@
 //
 // Created by azvs on 24-11-29.
 //
+
+#include "../include/azvs_base.h"
+#include "../include/azvs_linked.h"
+
+#include <stdio.h>
+#include <string.h>
+
+//////////////////////////////////////////////////
+//     Linked 的实现
+//////////////////////////////////////////////////
+
+APLinked __a_linked_init(const size_t data_size, char *data_type) {
+    APLinked linked = NEW(ALinked);
+    linked->data_size = data_size;
+    linked->data_type = data_type;
+    linked->head = NULL;
+    linked->tail = NULL;
+    linked->count = 0;
+    return linked;
+}
+
+void __a_linked_free(APLinked *linked) {
+    if (linked == NULL || *linked == NULL) {
+        LOG_MSG(LOG_LEVEL_INFO, "尝试释放空链表");
+        return;
+    }
+    while ((*linked)->head != NULL) {
+        APLinkedNode del = (*linked)->head;
+        (*linked)->head = (*linked)->head->next;
+        DELETE(del);
+    }
+    DELETE(*linked);
+    *linked = NULL;
+}
+
+APLinkedNode __a_linked_node_init(void *data, const size_t data_size) {
+    APLinkedNode new_node = NEW(ALinkedNode);
+    new_node->data = NEW_SIZE(data_size);
+    memcpy(new_node->data, data, data_size);
+    new_node->prev = NULL;
+    new_node->next = NULL;
+    return new_node;
+}
+
+void a_linked_insert_head(APLinked linked, void *data) {
+    if (linked == NULL) {
+        LOG_MSG(LOG_LEVEL_ERROR, "链表为空");
+        return;
+    }
+    APLinkedNode new_node = __a_linked_node_init(data, linked->data_size);
+    if (linked->count == 0) {
+        linked->head = new_node;
+        linked->tail = new_node;
+        linked->count += 1;
+        return;
+    }
+    new_node->next = linked->head;
+    new_node->next->prev = new_node;
+    linked->head = new_node;
+    linked->count += 1;
+}
+
+void a_linked_insert_tail(APLinked linked, void *data) {
+    if (linked == NULL) {
+        LOG_MSG(LOG_LEVEL_ERROR, "链表为空");
+        return;
+    }
+    APLinkedNode new_node = __a_linked_node_init(data, linked->data_size);
+    if (linked->count == 0) {
+        linked->head = new_node;
+        linked->tail = new_node;
+        linked->count += 1;
+        return;
+    }
+    new_node->prev = linked->tail;
+    new_node->prev->next = new_node;
+    linked->tail = new_node;
+    linked->count += 1;
+}
+
+void a_linked_remove_head(APLinked linked) {
+    if (linked == NULL) {
+        LOG_MSG(LOG_LEVEL_ERROR, "链表为空");
+        return;
+    }
+    if (linked->count == 0) {
+        LOG_MSG(LOG_LEVEL_WARN, "链表中没有数据，无法删除数据");
+        return;
+    }
+    if (linked->count == 1) {
+        DELETE(linked->head);
+        linked->head = NULL;
+        linked->tail = NULL;
+        linked->count -= 1;
+        return;
+    }
+    APLinkedNode del = linked->head;
+    linked->head = del->next;
+    linked->head->prev = NULL;
+    DELETE(del);
+    linked->count -= 1;
+}
+
+void a_linked_remove_tail(APLinked linked) {
+    if (linked == NULL) {
+        LOG_MSG(LOG_LEVEL_ERROR, "链表为空");
+        return;
+    }
+    if (linked->count == 0) {
+        LOG_MSG(LOG_LEVEL_WARN, "链表中没有数据，无法删除数据");
+        return;
+    }
+    if (linked->count == 1) {
+        DELETE(linked->head);
+        linked->head = NULL;
+        linked->tail = NULL;
+        linked->count -= 1;
+        return;
+    }
+    APLinkedNode del = linked->tail;
+    linked->tail = del->prev;
+    linked->tail->next = NULL;
+    DELETE(del);
+    linked->count -= 1;
+}
+
+void a_linked_print(APLinked linked, void (*operate)(void *), const char *end) {
+    if (linked == NULL) {
+        LOG_MSG(LOG_LEVEL_ERROR, "链表为空");
+        return;
+    }
+    APLinkedNode node = linked->head;
+    while (node != NULL) {
+        operate(node->data);
+        node = node->next;
+    }
+    if (end) printf("%s", end);
+}
+
+void a_linked_print_info(APLinked linked) {
+    if (linked == NULL) {
+        LOG_MSG(LOG_LEVEL_ERROR, "链表为空");
+        return;
+    }
+    printf("\n");
+    printf("ALinked Information:\n");
+    printf("\tData size : %zu bytes\n", linked->data_size);
+    printf("\tData type : %s\n", linked->data_type);
+    printf("\tCount     : %zu\n", linked->count);
+    printf("\n");
+}
--- include/azvs_linked.h
+++ include/azvs_linked.h
@@ -5,4 +5,94 @@
 #ifndef AZVS_LINKED_H
 #define AZVS_LINKED_H
 
+//////////////////////////////////////////////////
+//     Linked 的实现
+//////////////////////////////////////////////////
+
+typedef struct azvs_linked_node {
+    // 存放链表数据
+    void *data;
+    // 指向上一个节点
+    struct azvs_linked_node *prev;
+    // 指向下一个节点
+    struct azvs_linked_node *next;
+} ALinkedNode, *APLinkedNode;
+
+typedef struct azvs_linked {
+    // 链表的头部节点
+    APLinkedNode head;
+    // 链表的尾部节点
+    APLinkedNode tail;
+    // 链表中元素的大小
+    size_t data_size;
+    // 链表中元素的类型
+    char *data_type;
+    // 链表的节点个数
+    size_t count;
+} ALinked, *APLinked;
+
+/**
+* @brief 初始化链表，使用 #define 简化
+* @param data_size 单个元素占用的大小 sizeof(type)
+* @param data_type 存入元素的数据类型 ElementType
+*/
+APLinked __a_linked_init(size_t data_size, char *data_type);
+
+#define a_linked_init(type) __a_linked_init(sizeof(type), #type)
+
+/**
+* @brief 释放链表，使用 #define 简化
+* 释放成功后，指针指向 NULL。使用宏是为了释放时不需要 &
+* @param linked 需要释放的链表
+*/
+void __a_linked_free(APLinked *linked);
+
+#define a_linked_free(linked) __a_linked_free(&(linked))
+
+/**
+* @brief 创建链表节点，拷贝操作
+* @param data 需要创建的数据
+* @param data_size 数据大小
+*/
+APLinkedNode __a_linked_node_init(void *data, size_t data_size);
+
+/**
+* @brief 在链表头部插入数据
+* @param linked 需要操作的链表
+* @param data 需要插入的数据
+*/
+void a_linked_insert_head(APLinked linked, void *data);
+
+/**
+* @brief 在链表尾部插入数据
+* @param linked 需要操作的链表
+* @param data 需要插入的数据
+*/
+void a_linked_insert_tail(APLinked linked, void *data);
+
+/**
+* @brief 删除链表头部数据
+* @param linked 需要操作的链表
+*/
+void a_linked_remove_head(APLinked linked);
+
+/**
+* @brief 删除链表尾部数据
+* @param linked 需要操作的链表
+*/
+void a_linked_remove_tail(APLinked linked);
+
+/**
+ * @brief 输出链表的每个元素
+ * @param linked 需要操作的线性表
+ * @param operate 回调函数，指定输出规则
+ * @param end 结束输出，可指定为 NULL
+ */
+void a_linked_print(APLinked linked, void (*operate)(void *), const char *end);
+
+/**
+* @brief 输出链表的基本信息
+* @param linked 需要操作的链表
+*/
+void a_linked_print_info(APLinked linked);
 #endif //AZVS_LINKED_H
