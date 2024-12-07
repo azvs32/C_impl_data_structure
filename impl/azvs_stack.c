f6c72575b27ed724377e580291a8ba0a1cfdf041
azvs.top@gmail.com <azvs.top@qq.com>
2024-12-07 23:21:37 +0800

    实现stack的init操作和free操作。（pop类型问题未解决）

--- impl/azvs_stack.c
+++ impl/azvs_stack.c
@@ -1,67 +1,80 @@
 #include "../include/azvs_stack.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
-int a_stack_init(AStack *stack, size_t total) {
-    stack->data = (void **) malloc(total * sizeof(void *));
-    if (stack->data == NULL) {
-        return -1; // 内存分配失败
+#include "../include/azvs_base.h"
+
+APStack __a_stack_init(ASTACK_BASE_T base_struct, size_t total, size_t data_size, char *data_type) {
+    APStack stack = NEW(AStack);
+    stack->base_struct = base_struct;
+    switch (base_struct) {
+        case ASTACK_USE_ALINEAR:
+            stack->data.linear = __a_linear_init(total, data_size, data_type);
+            break;
+        case ASTACK_USE_ALINKED:
+            stack->data.linked = __a_linked_init(data_size, data_type);
+            break;
     }
-    stack->size = 0;
-    stack->total = total;
-    return 0;
+    return stack;
 }
 
-void a_stack_free(AStack *stack) {
-    free(stack->data);
-    stack->size = 0;
-    stack->total = 0;
+void __a_stack_free(APStack *stack) {
+    __CHECK_WARN(stack==NULL || *stack==NULL, "尝试释放空栈", return);
+    switch ((*stack)->base_struct) {
+        case ASTACK_USE_ALINEAR:
+            a_linear_free((*stack)->data.linear);
+            break;
+        case ASTACK_USE_ALINKED:
+            a_linked_free((*stack)->data.linked);
+            break;
+    }
+    DELETE(*stack);
+    *stack = NULL;
 }
 
-int a_stack_push(AStack *stack, void *e) {
-    if (stack->size >= stack->total) {
-        size_t new_total = stack->total * 2;
-        void **new_data = realloc(stack->data, new_total * sizeof(void *));
-        if (new_data == NULL) {
-            return -1; // 内存分配失败
-        }
-        stack->data = new_data;
-        stack->total = new_total;
-    }
-    stack->data[stack->size++] = e;
-    return 0;
-}
 
-int a_stack_pop(AStack *stack, void *e) {
-    if (a_stack_is_empty(stack)) {
-        return -1;
-    }
-    memcpy(e, stack->data[--stack->size], sizeof(e));
-    return 0;
-}
 
-int a_stack_value(AStack *stack, void *e) {
-    if (a_stack_is_empty(stack)) {
-        return -1;
-    }
-    memcpy(e, stack->data[stack->size - 1], sizeof(e));
-    return 0;
-}
-
-int a_stack_is_empty(AStack *stack) { return stack->size == 0; }
-
-int a_stack_print(AStack *stack, void (*operation)(void *), char *end) {
-    if (stack->data == NULL) {
-        return -1;
-    }
-    for (int i = 0; i < stack->size; i++) {
-        operation(stack->data[i]);
-    }
-    printf("%s", end);
-    return 0;
-}
-
-size_t a_stack_get_size(AStack *stack) { return stack->size; }
-
-size_t a_stack_get_total(AStack *stack) { return stack->total; }
+// int a_stack_push(AStack *stack, void *e) {
+//     if (stack->size >= stack->total) {
+//         size_t new_total = stack->total * 2;
+//         void **new_data = realloc(stack->data, new_total * sizeof(void *));
+//         if (new_data == NULL) {
+//             return -1; // 内存分配失败
+//         }
+//         stack->data = new_data;
+//         stack->total = new_total;
+//     }
+//     stack->data[stack->size++] = e;
+//     return 0;
+// }
+//
+// int a_stack_pop(AStack *stack, void *e) {
+//     if (a_stack_is_empty(stack)) {
+//         return -1;
+//     }
+//     memcpy(e, stack->data[--stack->size], sizeof(e));
+//     return 0;
+// }
+//
+// int a_stack_value(AStack *stack, void *e) {
+//     if (a_stack_is_empty(stack)) {
+//         return -1;
+//     }
+//     memcpy(e, stack->data[stack->size - 1], sizeof(e));
+//     return 0;
+// }
+//
+// int a_stack_is_empty(AStack *stack) { return stack->size == 0; }
+//
+// int a_stack_print(AStack *stack, void (*operation)(void *), char *end) {
+//     if (stack->data == NULL) {
+//         return -1;
+//     }
+//     for (int i = 0; i < stack->size; i++) {
+//         operation(stack->data[i]);
+//     }
+//     printf("%s", end);
+//     return 0;
+// }
+//
+// size_t a_stack_get_size(AStack *stack) { return stack->size; }
+//
+// size_t a_stack_get_total(AStack *stack) { return stack->total; }
--- include/azvs_stack.h
+++ include/azvs_stack.h
@@ -1,81 +1,114 @@
 #ifndef AZVS_STACK_H
 #define AZVS_STACK_H
 
+#include "./azvs_linear.h"
+#include "./azvs_linked.h"
 #include <stddef.h>
 
-// 栈
-typedef struct {
-    void **data; // 存储栈元素的数组
-    size_t size; // 当前栈的大小
-    size_t total; // 栈的预期总容量
-} AStack;
+typedef enum azvs_stack_base_struct {
+    // 底层使用线性表实现
+    ASTACK_USE_ALINEAR,
+    // 底层使用链表实现
+    ASTACK_USE_ALINKED,
+} ASTACK_BASE_T;
+
+typedef union azvs_stack_data {
+    APLinear linear;
+    APLinked linked;
+} ASTACK_DATA;
+
+typedef struct azvs_stack {
+    // 指定底层使用的结构
+    ASTACK_BASE_T base_struct;
+    // 存放数据
+    ASTACK_DATA data;
+} AStack, *APStack;
 
 /**
- * 栈初始化操作
- * @param stack 指定栈地址
- * @param total 指定栈的预期总容量，如果在压栈过程中发现总容量不足，会自动增大
- * @return 成功创建返回 0
- */
-int a_stack_init(AStack *stack, size_t total);
+* @brief 初始化栈
+* @param base_struct 指定栈底层的数据结构
+* @param total 若底层结构是线性表，则为栈的最大容量
+* @param data_size 指定栈中存储的元素大小
+* @param data_type 指定栈中存储的元素类型
+*/
+APStack __a_stack_init(ASTACK_BASE_T base_struct, size_t total, size_t data_size, char *data_type);
+
+#define a_stack_init(...) \
+    FUNCTION_OVERLOADING(__VA_ARGS__, _4, _3, a_stack_init_by_linear, a_stack_init_by_linked)(__VA_ARGS__)
+#define a_stack_init_by_linked(type)  __a_stack_init(ASTACK_USE_ALINKED, 0, sizeof(type), #type)
+#define a_stack_init_by_linear(total, type)  __a_stack_init(ASTACK_USE_ALINEAR, total, sizeof(type), #type)
 
 /**
- * 清空栈操作
- * @param stack 指定栈地址
- */
-void a_stack_free(AStack *stack);
+* @brief 释放栈
+* 释放成功后，指针将指向NULL
+* @param stack 需要操作的栈
+*/
+void __a_stack_free(APStack *stack);
+
+#define a_stack_free(stack) __a_stack_free(&(stack))
 
 /**
- * 入栈操作
- * @param stack 指定栈地址
- * @param e 指定需要压栈的元素地址
- * @return 成功入栈返回 0
- */
-int a_stack_push(AStack *stack, void *e);
+* @brief 入栈操作
+* @param stack 需要操作的栈
+* @param data 需要入栈的元素
+*/
+void a_stack_push(APStack stack, void *data);
 
 /**
- * 出栈操作
- * @param stack 指定栈地址
- * @param e 保存出栈的元素地址
- * @return 成功出栈返回 0
- */
-int a_stack_pop(AStack *stack, void *e);
+* @brief 出栈操作
+* @param stack 需要操作的栈
+*/
+void *ap_stack_pop(APStack stack);
+
+// #define a_stack_pop(stack, type) (*((type*)ap_stack_pop(stack)))
+// #define a_stack_pop(stack) \
+//     ({ \
+//         const char *data_type = (stack)->data.linear->data_type; \
+//         (typeof(*data_type) *)ap_stack_pop(stack); \
+//     })
 
 /**
- * 返回栈顶元素，该操作并不会使该元素出栈
- * @param stack 指定栈地址
- * @param e 保存栈定元素的地址
- * @return 成功则返回 0
- */
-int a_stack_value(AStack *stack, void *e);
+* @brief 查看栈顶元素
+* @param stack 需要操作的栈
+*/
+void *ap_stack_peek(APStack stack);
 
-/**
- * 检查栈是否为空
- * @param stack 指定栈地址
- * @return 如果栈内没有存储，则返回真
- */
-int a_stack_is_empty(AStack *stack);
-
-/**
- * 打印栈中的所有元素，先出栈的元素排在后面
- * @param stack 指定栈地址
- * @param operation 指定输出规则
- * @param end 自定义结束符号，若为NULL，则不定义。通常可以指定为"\n"
- * @return 操作成功返回 0
- */
-int a_stack_print(AStack *stack, void (*operation)(void *), char *end);
-
-/**
- * 获取栈的大小
- * @param stack 指定栈地址
- * @return 获取栈当前已经存入数据的大小
- */
-size_t a_stack_get_size(AStack *stack);
-
-/**
- * 获取栈的最大容量
- * @param stack 指定栈地址
- * @return 获取栈当前总容量
- */
-size_t a_stack_get_total(AStack *stack);
+///**
+// * 返回栈顶元素，该操作并不会使该元素出栈
+// * @param stack 指定栈地址
+// * @param e 保存栈定元素的地址
+// * @return 成功则返回 0
+// */
+//int a_stack_value(AStack *stack, void *e);
+//
+///**
+// * 检查栈是否为空
+// * @param stack 指定栈地址
+// * @return 如果栈内没有存储，则返回真
+// */
+//int a_stack_is_empty(AStack *stack);
+//
+///**
+// * 打印栈中的所有元素，先出栈的元素排在后面
+// * @param stack 指定栈地址
+// * @param operation 指定输出规则
+// * @param end 自定义结束符号，若为NULL，则不定义。通常可以指定为"\n"
+// * @return 操作成功返回 0
+// */
+//int a_stack_print(AStack *stack, void (*operation)(void *), char *end);
+//
+///**
+// * 获取栈的大小
+// * @param stack 指定栈地址
+// * @return 获取栈当前已经存入数据的大小
+// */
+//size_t a_stack_get_size(AStack *stack);
+//
+///**
+// * 获取栈的最大容量
+// * @param stack 指定栈地址
+// * @return 获取栈当前总容量
+// */
+//size_t a_stack_get_total(AStack *stack);
 
 #endif // !AZVS_STACK_H
